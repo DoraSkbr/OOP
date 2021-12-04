7ca60ee483bc458179b7daf5fa75c12eefbc27e7
DoraSkbr <94116430+DoraSkbr@users.noreply.github.com>
2021-12-04 15:21:44 +0300

    Create 1_2.cpp

---
+++ lab6/1_2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class DomesticAnimal
+{
+protected:
+    double weight;
+    double price;
+    string color;
+
+public:
+    DomesticAnimal() {}                                                             // Конструктор без параметров
+    DomesticAnimal(double w, double p, string c) : weight(w), price(p), color(c) {} // Конструктор с параметрами
+    void print()                                                                    // Вывод значения полей класса
+    {
+        cout << "DomesticAnimal:" << endl;
+        cout << "weight: " << weight << endl
+             << "price: " << price << endl
+             << "color: " << color << endl;
+    }
+};
+
+class Cow : virtual public DomesticAnimal // Виртуальное наследование
+{
+public:
+    Cow() {}                                                       // Конструктор без параметров
+    Cow(double w, double p, string c) : DomesticAnimal(w, p, c) {} // Конструктор с параметрами
+    void print()                                                   // Вывод значения полей класса
+    {
+        cout << "Cow->";
+        DomesticAnimal::print(); // Наследование метода от родительского класса
+    }
+};
+
+class Buffalo : virtual public DomesticAnimal // Виртуальное наследование
+{
+public:
+    Buffalo() {}                                                       // Конструктор без параметров
+    Buffalo(double w, double p, string c) : DomesticAnimal(w, p, c) {} // Конструктор с параметрами
+    void print()                                                       // Вывод значения полей класса
+    {
+        cout << "Buffalo->";
+        DomesticAnimal::print(); // Наследование метода от родительского класса
+    }
+};
+
+class Beefalo : public Cow, public Buffalo
+{
+public:
+    Beefalo() {}                                                       // Конструктор без параметров
+    Beefalo(double w, double p, string c) : DomesticAnimal(w, p, c) {} // Конструктор с параметрами
+    void print()                                                       // Вывод значения полей класса
+    {
+        cout << "Beefalo->";
+        DomesticAnimal::print(); // Наследование метода от родительского класса
+    }
+};
+
+int main()
+{
+    DomesticAnimal dir(1, 100, "red"); // Инициализация объекта класса DomesticAnimal с параметрами
+    Cow co(10, 200, "withe");          // Инициализация объекта класса Cow с параметрами
+    Buffalo buf(20, 250, "black");     // Инициализация объекта класса Buffalo с параметрами
+    Beefalo bef(12, 450, "pink");      // Инициализация объекта класса Beefalo с параметрами
+    dir.print();
+    co.print();
+    buf.print();
+    bef.print();
+}
